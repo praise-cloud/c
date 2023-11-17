6d247be0c4ce7114a19c80e15ceb811b3fa793d5
Praise Georgewill <georgewillpraise15@gmail.com>
2023-11-18 00:09:49 +0100

    First commit

---
+++ hello.c
@@ -0,0 +1,8 @@
+// source code in c, that is the general theme that programmers write that is changed into machine code (binary)
+
+#include <stdio.h>
+
+int main(void)
+{
+    printf("hello, world\n");
+}
