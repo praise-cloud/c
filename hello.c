cf2814b0b12bf4f0071f120e84bf8e179778efc5
Praise Georgewill <georgewillpraise15@gmail.com>
2023-11-18 00:46:09 +0100

    pushing again

--- hello.c
+++ hello.c
@@ -1,8 +1,14 @@
 // source code in c, that is the general theme that programmers write that is changed into machine code (binary)
 
-#include <stdio.h>// standard io.h
+#include <stdio.h>// standard io.h, this is importing the standard.io library that gives us access to printf
 
-int main(void)
+int main() //
 {
-    printf("hello, world\n"); // this will print a formatted value to the terminal
-}
+    char name[]= "praise";
+
+    printf("hello, world %s\n", name); // this will print a formatted value to the terminal
+
+    int number = 15;
+
+    printf("hello %d", number); // $d i for number, %s is for strings etc
+}       
