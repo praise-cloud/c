d1064488f7cefe06d35837ce18ed48fea7b6ceb8
Praise Georgewill <georgewillpraise15@gmail.com>
2023-11-18 01:32:33 +0100

    pushing another form of the while loop

--- functions.c
+++ functions.c
@@ -2,13 +2,21 @@
 
 int main(void){
     // loops and varaibles
-    int counting = 3;
-    while(counting > 0)
+    // int counting = 3;
+    // while(counting > 0)
+    // {
+    //     printf("meow\n");
+         // counting = counting - 1;
+    //     counting--;
+    // }
+
+    int i = 0;
+    while(i < 3)
     {
         printf("meow\n");
-        // counting = counting - 1;
-        counting--;
+        i++;
     }
+
 
     //for loop
     for(int counter = 0; counter < 3; counter++){
